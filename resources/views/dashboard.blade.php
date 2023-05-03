3bd92a959627c41eeb930c94e22dbe554702fda0
Daniel Raab <raab.daniel@fronius.com>
2023-05-03 13:45:37 +0200

    guest layout uses app layout; extract some navigation and make it guest usable

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @auth
+                        {{ __("You're logged in!") }}
+                    @endauth
+
+                    @guest
+                        {{ __("You are a guest !!!") }}
+                    @endguest
                 </div>
             </div>
         </div>
