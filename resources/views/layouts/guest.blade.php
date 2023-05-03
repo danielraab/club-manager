1e2142b1bb3bd895906e1a139ab31b90c1e400ba
Daniel Raab <raab.daniel@fronius.com>
2023-05-03 14:07:39 +0200

    add mvk logo as application logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,7 +2,7 @@
         <div class="min-h-[80vh] flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
