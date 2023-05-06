bec12e74ea6c3a25b0a987709977dafd2a492d98
Daniel Raab <daniel@draab.at>
2023-05-07 01:05:17 +0200

    add singel admin user to seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->singleAdminUser();
+    }
+
+    private function singleAdminUser(): void
+    {
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'tester@draab.at',
+         ]);
     }
 }
