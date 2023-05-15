a99f1e9caa77615958ff202ecfd12d8350a4205b
Daniel Raab <daniel@draab.at>
2023-05-15 20:33:32 +0200

    add backend template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,15 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+<x-backend-layout>
+    <x-slot name="headline">
+        {{ __('Dashboard') }}
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    @auth
-                        {{ __("You're logged in!") }}
-                    @endauth
+    <div class="p-6 text-gray-900">
+        @auth
+            {{ __("You're logged in!") }}
+        @endauth
 
-                    @guest
-                        {{ __("You are a guest !!!") }}
-                    @endguest
-                </div>
-            </div>
-        </div>
+        @guest
+            {{ __("You are a guest !!!") }}
+        @endguest
     </div>
-</x-app-layout>
+</x-backend-layout>
