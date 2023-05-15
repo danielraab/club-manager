baa8b083ec96f5e639997b1d901a80a09070cd63
Daniel Raab <raab.daniel@fronius.com>
2023-05-15 15:08:50 +0200

    update factory for user; password is mail

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => $email,
             'email_verified_at' => now(),
-            'password' => Hash::make($email), // password
+            'password' => Hash::make($email),
             'remember_token' => Str::random(10),
         ];
     }
