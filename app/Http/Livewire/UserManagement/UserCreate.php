4b1d7f38ce027eeef0c43ce4a00778d74fd10a43
Daniel Raab <raab.daniel@fronius.com>
2023-05-17 17:07:10 +0200

    permissions for create user form

--- app/Http/Livewire/UserManagement/UserCreate.php
+++ app/Http/Livewire/UserManagement/UserCreate.php
@@ -8,14 +8,16 @@ use Livewire\Component;
 
 class UserCreate extends Component
 {
+    use UserPermissionComponentTrait;
+
     public User $user;
 
     protected function rules()
     {
-        return [
+        return array_merge([
             'user.name' => ['required', 'string', 'min:5'],
             'user.email' => ['required', 'string', 'email', 'unique:users,email'],
-        ];
+        ], $this->permissionRules());
     }
 
     public function mount()
@@ -28,9 +30,12 @@ class UserCreate extends Component
         $this->validate();
 
         $this->user->register();
+        $this->user->userPermissions()->sync(
+            $this->getSelectedPermissionKeys()
+        );
 
         session()->put('message', __("User '".$this->user->name."' created successfully."));
-        Log::channel('userManagement')->info("User '".$this->user->getNameWithMail().">' has been created by '".auth()->user()->getNameWithMail()."'");
+        Log::channel('userManagement')->info("User '".$this->user->getNameWithMail()."' has been created by '".auth()->user()->getNameWithMail()."'");
         $this->redirect(route('userManagement.index'));
     }
 
--- resources/views/livewire/user-management/user-create.blade.php
+++ resources/views/livewire/user-management/user-create.blade.php
@@ -12,9 +12,14 @@
             </div>
         </div>
     </div>
-    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-5">
         <div class="p-4 sm:p-8 max-w-xl">
             <x-livewire.user-management.user-form/>
+        </div>
+    </div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-4 sm:p-8">
+            <x-livewire.user-management.user-permission/>
         </div>
     </div>
 </div>
