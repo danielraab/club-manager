64cd1927ee166b8a5f9bcfc532d9cf6f484faf87
Daniel Raab <raab.daniel@fronius.com>
2023-05-22 08:41:06 +0200

    load events on dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -2,23 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use App\Models\News;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
 
 class Dashboard extends Controller
 {
     public function index()
     {
+
+        return view('dashboard', [
+            'newsList' => $this->loadNewsList(),
+            'eventList' => $this->loadEventList()
+        ]);
+    }
+
+    private function loadNewsList(): Collection
+    {
         $newsList = News::orderBy('display_until', 'desc');
         $newsList = $newsList->where("display_until", '>', now());
         $newsList = $newsList->where("enabled", true);
-        if(!Auth::user()) {
+        if (!Auth::user()) {
             $newsList = $newsList->where("logged_in_only", false);
         }
-        $newsList = $newsList->get();
+        return $newsList->get();
+    }
 
-        return view('dashboard', [
-            'newsList' => $newsList
-        ]);
+    private function loadEventList()
+    {
+        $eventList = Event::orderBy("start", "desc");
+        $eventList = $eventList->where("start", '>', now());
+        $eventList = $eventList->where("enabled", true);
+        if (!Auth::user()) {
+            $eventList = $eventList->where("logged_in_only", false);
+        }
+        return $eventList->get();
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,20 +5,14 @@
 
 
     <div class="flex flex-col-reverse lg:grid lg:grid-cols-2 gap-4">
-        <div class="bg-white shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
-                {{\Illuminate\Support\Facades\App::currentLocale()}}
-                @auth
-                    {{ __("You're logged in!") }}
-                @endauth
-
-                @guest
-                    {{ __("You are a guest !!!") }}
-                @endguest
-            </div>
+        <div class="bg-white shadow-sm sm:rounded-lg p-4">
+            @foreach($eventList as $event)
+                <div>{{$event->title}} - todo</div>
+            @endforeach
         </div>
         <div class="bg-white shadow-sm sm:rounded-lg p-4">
             <x-dashboard.newsList :newsList="$newsList"/>
         </div>
+
     </div>
 </x-backend-layout>
