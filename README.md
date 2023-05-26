e871a7b791248b669ed8adf33b00777bdc3fd4c8
Daniel Raab <daniel@draab.at>
2023-05-26 22:28:38 +0200

    add deployer; rename project to club schulder

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Mvk Intern
+# club scheduler
 
 ## install repo
 
---
+++ deploy.php
@@ -0,0 +1,50 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'git@gitlab.com:draab_/web/club-scheduler.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('intern.mv-kematen.at')
+    ->set('remote_user', 'hosting187061')
+    ->set('http_user', 'hosting187061')
+    ->set('bin/php', '/usr/local/php82/bin/php')
+    ->set('writable_mode', 'chmod')
+    ->set('deploy_path', '~/intern.mv-kematen.at/httpdocs');
+
+
+// tasks
+task('config:cache:fix', function () {
+    cd('{{release_or_current_path}}');
+    run("sed -i -- 's/\/intern.mv-kematen.at\/httpdocs\//\/var\/www\/vhosts\/hosting187061.a2ea9.netcup.net\/intern.mv-kematen.at\/httpdocs\//g' ./bootstrap/cache/*");
+});
+
+
+/**
+ * Main deploy task.
+ */
+desc('Deploys your project');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:config:cache', // cli realpath is not the same like apache realpath
+    'artisan:route:cache',
+    'artisan:view:cache',
+    'artisan:event:cache',
+    'config:cache:fix',
+    'artisan:migrate',
+    'deploy:publish',
+]);
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
