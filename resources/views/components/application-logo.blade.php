f9e9d90a8b6d4732faf44482c8f2915b6446368f
Daniel Raab <daniel@draab.at>
2023-05-27 10:07:06 +0200

    fix wrong resource path in logo component

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src = "{{\Illuminate\Support\Facades\Vite::asset("/resources/images/logo.svg")}}" alt="Logo"  {{ $attributes }}/>
+<img src = "{{\Illuminate\Support\Facades\Vite::asset("resources/images/logo.svg")}}" alt="Logo"  {{ $attributes }}/>
