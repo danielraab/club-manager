53085d329821f3b604bb2f8f163ad847f5115de6
Daniel Raab <daniel@draab.at>
2023-05-31 12:50:42 +0200

    allow diable old events only with permissions

--- app/Http/Livewire/Events/EventOverview.php
+++ app/Http/Livewire/Events/EventOverview.php
@@ -21,7 +21,7 @@ class EventOverview extends Component
     public function toggleEnabledState(Event $event)
     {
         if (Auth::user()?->hasPermission(Event::EVENT_EDIT_PERMISSION)) {
-            $event->enabled = ! $event->enabled;
+            $event->enabled = !$event->enabled;
             $event->lastUpdater()->associate(Auth::user());
             $event->save();
         } else {
@@ -31,9 +31,13 @@ class EventOverview extends Component
 
     public function disableLastYearEvents()
     {
-        $cnt = Event::where("end", "<", now()->setMonth(0)->setDay(0)->setTime(0,0,0))
-            ->update(["enabled"=> false]);
-        session()->flash("eventDisableMessage", __("Done. :cnt Event(s) affected.",["cnt"=>$cnt]));
+        if (Auth::user()?->hasPermission(Event::EVENT_EDIT_PERMISSION)) {
+            $cnt = Event::where("end", "<", now()->setMonth(0)->setDay(0)->setTime(0, 0, 0))
+                ->update(["enabled" => false]);
+            session()->flash("eventDisableMessage", __("Done. :cnt Event(s) affected.", ["cnt" => $cnt]));
+        } else {
+            abort(403);
+        }
     }
 
     public function render()
@@ -49,7 +53,7 @@ class EventOverview extends Component
         if (Auth::guest()) {
             $eventList = $eventList->where('logged_in_only', false);
             $eventList = $eventList->where('enabled', true);
-        } elseif (! Auth::user()->hasPermission(Event::EVENT_EDIT_PERMISSION)) {
+        } elseif (!Auth::user()->hasPermission(Event::EVENT_EDIT_PERMISSION)) {
             $eventList = $eventList->where('enabled', true);
         }
 
