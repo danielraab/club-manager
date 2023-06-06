7925d5f390b9a612f273b21d2ab8a8ec29d0809f
Daniel Raab <daniel@draab.at>
2023-06-06 20:46:25 +0200

    set new version number

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
-    'version' => 'v0.0.3',
+    'version' => 'v0.0.4',
 
     /*
     |--------------------------------------------------------------------------
