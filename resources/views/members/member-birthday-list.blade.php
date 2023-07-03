d52d735081e7f430e37224521a2d9cf22490f421
Daniel Raab <daniel@draab.at>
2023-07-04 00:18:22 +0200

    hide missing birthday section if nothing missing, on birthday list

--- resources/views/members/member-birthday-list.blade.php
+++ resources/views/members/member-birthday-list.blade.php
@@ -6,44 +6,49 @@
     </x-slot>
 
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900">
-        <x-always-responsive-table class="table-auto mx-auto text-center">
-            <thead class="font-bold">
-            <tr>
-                <td class="px-4 py-2">Name</td>
-                <td class="px-4 py-2">{{__("Birthday")}}</td>
-                <td class="px-4 py-2">{{__("Age")}}</td>
-            </tr>
-            </thead>
-            <tbody>
-            @php($today = now()->format("m-d"))
-            @php($todayDisplayed = false)
-            @foreach($members as $member)
-                @php($current = $member->birthday->format("m-d"))
-                @if(!$todayDisplayed && strcmp($today, $current) <= 0)
-                    <tr>
-                        <td colspan="3">
-                            <div class="relative py-4">
-                                <div class="absolute inset-0 flex items-center">
-                                    <div class="w-full border-b border-black"></div>
-                                </div>
-                                <div class="relative flex justify-center">
-                                    <span class="bg-indigo-700 text-white px-4 border border-black rounded">Today - {{now()->isoFormat("D. MMMM")}}</span>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    @php($todayDisplayed = true)
-                @endif
-                <tr class="[&:nth-child(2n)]:bg-indigo-200">
-                    <td class="border px-4 py-2">{{ $member->lastname }} {{ $member->firstname }}</td>
-                    <td class="border px-4 py-2">{{ $member->birthday->isoFormat("D. MMMM") }}</td>
-                    <td class="border px-4 py-2">{{ now()->format("Y") - $member->birthday->format("Y") }}</td>
+        @if($members && $members->isNotEmpty())
+            <x-always-responsive-table class="table-auto mx-auto text-center">
+                <thead class="font-bold">
+                <tr>
+                    <td class="px-4 py-2">Name</td>
+                    <td class="px-4 py-2">{{__("Birthday")}}</td>
+                    <td class="px-4 py-2">{{__("Age")}}</td>
                 </tr>
-            @endforeach
-            </tbody>
-        </x-always-responsive-table>
+                </thead>
+                <tbody>
+                @php($today = now()->format("m-d"))
+                @php($todayDisplayed = false)
+                @foreach($members as $member)
+                    @php($current = $member->birthday->format("m-d"))
+                    @if(!$todayDisplayed && strcmp($today, $current) <= 0)
+                        <tr>
+                            <td colspan="3">
+                                <div class="relative py-4">
+                                    <div class="absolute inset-0 flex items-center">
+                                        <div class="w-full border-b border-black"></div>
+                                    </div>
+                                    <div class="relative flex justify-center">
+                                        <span
+                                            class="bg-indigo-700 text-white px-4 border border-black rounded">Today - {{now()->isoFormat("D. MMMM")}}</span>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        @php($todayDisplayed = true)
+                    @endif
+                    <tr class="[&:nth-child(2n)]:bg-indigo-200">
+                        <td class="border px-4 py-2">{{ $member->lastname }} {{ $member->firstname }}</td>
+                        <td class="border px-4 py-2">{{ $member->birthday->isoFormat("D. MMMM") }}</td>
+                        <td class="border px-4 py-2">{{ now()->format("Y") - $member->birthday->format("Y") }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </x-always-responsive-table>
+        @else
+            <div class="w-full text-center">{{__("No members")}}</div>
+        @endif
     </div>
-    @if($missingBirthdayList)
+    @if($missingBirthdayList && $missingBirthdayList->isNotEmpty())
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900 mt-5">
 
             <header>
