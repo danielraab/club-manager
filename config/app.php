efd4032c3c70ae218585199d9bb8d8a5a1740d1f
Daniel Raab <daniel@draab.at>
2023-07-03 12:55:09 +0200

    update version and assets

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
-    'version' => 'v0.0.7',
+    'version' => 'v0.1.0',
 
     /*
     |--------------------------------------------------------------------------
