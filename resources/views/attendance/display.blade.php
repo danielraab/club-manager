71adbefd6f2ff8b19418dd5cbb14ec4ad0f41402
Daniel Raab <daniel@draab.at>
2023-07-24 02:57:45 +0200

    show all members (incl without attendance info) on attendance overview

--- resources/views/attendance/display.blade.php
+++ resources/views/attendance/display.blade.php
@@ -85,19 +85,20 @@
         <div class="flex bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900 justify-center">
             <div>
                 @if(request('displayType') === 'list')
-                    @forelse($event->attendances()->get() as $attendance)
+                    @forelse($members = \App\Models\Member::allActive(true)->get() as $member)
                         @php
+                            /** @var \App\Models\Member $member */
+                            $attendance = $member->attendances()->where("event_id", $event->id)->first();
                             /** @var \App\Models\Attendance $attendance */
-                                $cssClasses = $attendance->attended ? " bg-green-300" : '';
+                            $cssClasses = $attendance?->attended ? " bg-green-300" : '';
                         @endphp
                         <div class="flex gap-2 items-center px-2">
-                            <div class="h-2 w-2 rounded-full {{match($attendance->poll_status){
+                            <div class="h-2 w-2 rounded-full {{match($attendance?->poll_status){
                         "in" => 'bg-green-700',
                         "unsure" => 'bg-yellow-600',
                         "out" => 'bg-red-700',
                         default => ''} }}"></div>
-                            <span
-                                class="rounded px-2 {{$cssClasses}}">{{__($attendance->member()->first()->getFullName())}}</span>
+                            <span class="rounded px-2 {{$cssClasses}}">{{__($member->getFullName())}}</span>
                         </div>
 
                     @empty
