c3fef4a296a1f5b29efa27729735db33edb73592
Daniel Raab <daniel@draab.at>
2023-08-06 03:10:36 +0200

    fix duplicate routeName

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
-    'version' => 'v0.3.0',
+    'version' => 'v0.3.1',
 
     /*
     |--------------------------------------------------------------------------
--- routes/webPush.php
+++ routes/webPush.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/webPush/hasEndpoint', [\App\Http\Controllers\WebPush::class, 'hasEndpoint'])
     ->name('webPush.hasEndpoint');
 Route::post('/webPush/removeEndpoint', [\App\Http\Controllers\WebPush::class, 'removeEndpoint'])
-    ->name('webPush.hasEndpoint');
+    ->name('webPush.removeEndpoint');
 Route::post('/webPush', [\App\Http\Controllers\WebPush::class, 'store'])
     ->name('webPush.store');
