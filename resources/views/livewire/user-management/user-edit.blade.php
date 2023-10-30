583628cdf9228aad59630222931527ab3705d603
Daniel Raab <daniel@draab.at>
2023-10-30 06:14:06 +0100

    place button

--- resources/views/livewire/user-management/user-edit.blade.php
+++ resources/views/livewire/user-management/user-edit.blade.php
@@ -6,18 +6,33 @@
 
 <div class="space-y-4">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-5 p-5">
-        <div class="flex items-center justify-between">
-            <x-default-button
-                x-data="{ clickCnt: 0, onClick() {
-                if(this.clickCnt > 0) {
-                    $wire.deleteUser();
-                } else {
-                    this.clickCnt++;
-                    $el.innerHTML = 'Are you sure?';
-                }
-            }}"
-                x-on:click="onClick()" title="Delete this user"
-                class="btn-danger">{{ __('Delete user') }}</x-default-button>
+        <div class="flex gap-1 items-center justify-between">
+            <div>
+                <div class="flex flex-wrap grow-0 gap-1 justify-center">
+                    <x-default-button
+                        x-data="{ clickCnt: 0, onClick() {
+                        if(this.clickCnt > 0) {
+                            $wire.deleteUser();
+                        } else {
+                            this.clickCnt++;
+                            $el.innerHTML = 'Are you sure?';
+                        }
+                    }}"
+                        x-on:click="onClick()" title="Delete this user"
+                        class="btn-danger">{{ __('Delete user') }}</x-default-button>
+                    <x-default-button
+                        x-data="{ clickCnt: 0, onClick() {
+                        if(this.clickCnt > 0) {
+                            $wire.sendResetLink();
+                        } else {
+                            this.clickCnt++;
+                            $el.innerHTML = 'Are you sure?';
+                        }
+                    }}"
+                        x-on:click="onClick()" title="Send reset link"
+                        class="btn-secondary">{{ __('Send reset link') }}</x-default-button>
+                </div>
+            </div>
             <x-default-button class="btn-primary" wire:click="saveUser"
                               title="Save the current changes">{{ __('Save') }}</x-default-button>
         </div>
@@ -37,7 +52,8 @@
                 @if($userForm->user->last_login_at)
                     <div class="text-gray-500 mt-3 ml-3 space-x-2">
                         <i class="fa-solid fa-right-to-bracket"></i>
-                        <span title="{{__("Last login at")}}">{{$userForm->user->last_login_at?->formatDateTimeWithSec()}}</span>
+                        <span
+                            title="{{__("Last login at")}}">{{$userForm->user->last_login_at?->formatDateTimeWithSec()}}</span>
                     </div>
                 @endif
             </div>
