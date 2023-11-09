4cd284f3599f9cd3c886ddd705391d6618e147b9
Daniel Raab <daniel@draab.at>
2023-11-09 01:18:25 +0100

    add edit button to dashboard news

--- resources/views/components/dashboard/newsList.blade.php
+++ resources/views/components/dashboard/newsList.blade.php
@@ -1,3 +1,7 @@
+@php
+    /** @var \App\Models\News $news */
+    $hasEditPermission = \Illuminate\Support\Facades\Auth::user()?->hasPermission(\App\Models\News::NEWS_EDIT_PERMISSION) ?? false;
+@endphp
 <div class="flex flex-col gap-2">
 
     @forelse($newsList as $news)
@@ -5,6 +9,18 @@
         <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
             <p class="font-bold">{{$news->title}}</p>
             <p class="text-sm">{!! $news->content !!}</p>
+
+            <div class="flex justify-end items-center gap-1">
+                @if($news->logged_in_only)
+                    <i class="fa-solid fa-arrow-right-to-bracket text-sm text-gray-600 mr-2"
+                       title="{{__("Visible only for logged in users")}}"></i>
+                @endif
+                @if($hasEditPermission)
+                    <a href="{{route('news.edit', $news->id)}}" title="Edit this news">
+                        <i class="fa-regular fa-pen-to-square"></i>
+                    </a>
+                @endif
+            </div>
         </div>
 
     @empty
