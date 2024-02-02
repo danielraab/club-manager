c7eaf4c62024b81bea30851a68163e52e635e0e2
Daniel Raab <daniel@draab.at>
2024-02-02 23:37:39 +0100

    add config for public poll member filter

---
+++ app/Livewire/Settings/Polls.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Settings;
+
+use App\Models\Configuration;
+use App\Models\ConfigurationKey;
+use Livewire\Attributes\Renderless;
+use Livewire\Component;
+
+class Polls extends Component
+{
+    #[Renderless]
+    public function setBeforeEntrance(bool $value):void {
+        Configuration::storeBool(ConfigurationKey::POLL_PUBLIC_FILTER_BEFORE_ENTRANCE, $value);
+    }
+    #[Renderless]
+    public function setAfterRetired(bool $value):void {
+        Configuration::storeBool(ConfigurationKey::POLL_PUBLIC_FILTER_AFTER_RETIRED, $value);
+    }
+    #[Renderless]
+    public function setShowPaused(bool $value):void {
+        Configuration::storeBool(ConfigurationKey::POLL_PUBLIC_FILTER_SHOW_PAUSED, $value);
+    }
+
+    public function render(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('livewire.settings.polls');
+    }
+}
