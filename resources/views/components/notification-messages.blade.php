5d77821e82ce525ecc866f3139fc164b7b6271e5
Daniel Raab <daniel@draab.at>
2024-03-08 12:48:40 +0100

    add negative progress bar

--- resources/views/components/notification-messages.blade.php
+++ resources/views/components/notification-messages.blade.php
@@ -1,11 +1,3 @@
-@php
-        \App\Facade\NotificationMessage::addNotificationMessage(new \App\NotificationMessage\Item("test",displayedSeconds: 2));
-        \App\Facade\NotificationMessage::addNotificationMessage(new \App\NotificationMessage\Item("test message", \App\NotificationMessage\ItemType::SUCCESS, "test title",displayedSeconds: 5));
-        \App\Facade\NotificationMessage::addNotificationMessage(new \App\NotificationMessage\Item("test3 a very long message could lead to a mobile problem, therfore i write this longer notifigation message", \App\NotificationMessage\ItemType::ERROR,displayedSeconds: 8));
-        \App\Facade\NotificationMessage::addNotificationMessage(new \App\NotificationMessage\Item("test4", \App\NotificationMessage\ItemType::WARNING,displayedSeconds: 12));
-//        dd(\App\Facade\NotificationMessage::popNotificationMessagesJson());
-@endphp
-
 <script>
     document.addEventListener('alpine:init', () => {
         Alpine.store('notificationMessages', {
@@ -19,19 +11,32 @@
                 delete this.messages[timestamp];
             }
         });
-        console.log("add messages");
         Alpine.store('notificationMessages').addNotificationMessages(
             JSON.parse('<?= \App\Facade\NotificationMessage::popNotificationMessagesJson() ?>'));
     });
 </script>
 
-<div x-data class="fixed overflow-auto top-3 right-0 max-w-7xl sm:px-6 lg:px-8 space-y-1">
+<div x-data class="fixed overflow-auto top-3 right-0 max-w-7xl sm:px-6 lg:px-8 space-y-2">
     <template x-for="message in $store.notificationMessages.messages" :key="message.timestamp">
-        <div x-data="{show:true, hideAndRemove() {
-            this.show = false;
-            setTimeout(()=>$store.notificationMessages.removeNotificationMessage(message.timestamp), 1000);
-        }}"
-             x-init="setTimeout(hideAndRemove, message.displayedSeconds*1000)"
+        <div x-data="{
+            show:true, progress: 100, shrinkInterval:undefined,
+            getProgressStyle(){return 'width:'+this.progress+'%'},
+            hideAndRemove() {
+                this.show = false;
+                clearInterval(this.shrinkInterval);
+                setTimeout(()=>$store.notificationMessages.removeNotificationMessage(message.timestamp), 1000);
+            },
+            startShrinkProgress() {
+                this.shrinkInterval = setInterval(()=>{
+                    this.progress = this.progress - ((100*100) / (message.displayedSeconds*1000));
+                    if(this.progress <= 0) {
+                        this.progress = 0
+                        this.hideAndRemove();
+                    }
+                },100);
+            },
+        }"
+             x-init="startShrinkProgress()"
              x-show="show" x-collapse>
             <div class="flex gap-4 items-center px-4 bg-white border border-t-4 shadow-xl opacity-90"
                  role="alert"
@@ -47,7 +52,7 @@
                 'fa-circle-check text-green-700': message.type === 'SUCCESS',
                 'fa-triangle-exclamation text-yellow-500': message.type === 'WARNING',
                 'fa-circle-xmark text-red-700': message.type === 'ERROR',
-                'fa-info-circle text-blue-700': message.type === 'INFORMATION',
+                'fa-info-circle text-blue-700': message.type === 'INFORMATION'
                 }"></i>
                 <div class="py-2">
                     <template x-if="message.title">
@@ -59,6 +64,14 @@
                 <button class="ml-auto" x-on:click="hideAndRemove()"><i class="fa-solid fa-xmark text-gray-500"></i>
                 </button>
             </div>
+            <div class="h-[3px] mx-auto"
+                :class="{
+                    'bg-green-700': message.type === 'SUCCESS',
+                    'bg-yellow-500': message.type === 'WARNING',
+                    'bg-red-700': message.type === 'ERROR',
+                    'bg-blue-700': message.type === 'INFORMATION'
+                    }"
+                :style="getProgressStyle()"></div>
         </div>
     </template>
 </div>
