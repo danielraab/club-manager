2602e0c168a5cf6c8d22be031135a6dfc019cc3b
Raab Daniel <raab.daniel@fronius.com>
2024-03-19 16:27:06 +0100

    remove sub nav for news (only one subnav"

--- resources/views/layouts/navigation/news.blade.php
+++ resources/views/layouts/navigation/news.blade.php
@@ -8,11 +8,4 @@
             {{ __('News Management') }}
         </x-responsive-nav-link>
     </x-slot>
-    @if($hasEditPermission)
-        <x-responsive-nav-link href="{{route('news.create')}}" title="Create new event"
-                               iconClasses="fa-solid fa-plus"
-                               :active="request()->routeIs('news.create')">
-            {{__("Create new news")}}
-        </x-responsive-nav-link>
-    @endif
 </x-nav-dropdown>
