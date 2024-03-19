a53de4b124c820d15c4f9d4118d1a499525b6e89
Raab Daniel <raab.daniel@fronius.com>
2024-03-19 16:16:50 +0100

    change nav dropdown click icon to caret"

--- resources/views/components/nav-dropdown.blade.php
+++ resources/views/components/nav-dropdown.blade.php
@@ -4,7 +4,7 @@
         <div class="flex flex-row justify-between items-center">
             {{$mainLink}}
             <button type="button" @click="open = !open" class="px-3">
-                <i class="fa-solid" :class="{'fa-plus':!open, 'fa-minus':open}"></i>
+                <i class="fa-solid fa-angle-down transition duration-500" :class="{'fa-rotate-180':open}"></i>
             </button>
         </div>
         <div x-show="open" x-collapse class="ml-6">
