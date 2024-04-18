5093f01c31bba7cba2933660013b6412dda5f235
Raab Daniel <raab.daniel@fronius.com>
2024-04-18 14:57:21 +0200

    small adaptions on new webPush and development page

--- resources/js/webPush.js
+++ resources/js/webPush.js
@@ -1,4 +1,5 @@
 const webPush = {
+    errors: [],
     notification: {
         isNotificationSupported() {
             return typeof Notification !== "undefined"
@@ -11,12 +12,18 @@ const webPush = {
         hasNotificationPermission() {
             return this.getNotificationApiPermissionStatus() === "granted";
         },
+        requestNotificationPermission: () => {
+            return Notification.requestPermission().then(result => {
+                return result === "granted";
+            });
+        }
     },
     serviceWorker: {
         isServiceWorkerSupported() {
             return "serviceWorker" in navigator;
         },
         hasServiceWorker() {
+            console.log('sw', !!navigator.serviceWorker.controller);
             return !!navigator.serviceWorker.controller;
         },
         getServiceWorkerScriptUrl() {
@@ -26,6 +33,66 @@ const webPush = {
             return navigator.serviceWorker.register('/sw.js')
         }
     },
+
+    vapid: {
+        isVapidPublicKeyStored: function () {
+            let vapidPublicKey = localStorage.getItem("vapidPublicKey");
+            return vapidPublicKey && vapidPublicKey.length > 0;
+        },
+
+        getStoredVapidPublicKey: function () {
+            return localStorage.getItem("vapidPublicKey");
+        },
+
+        setStoredVapidPublicKey: function (publicKey) {
+            localStorage.setItem("vapidPublicKey", publicKey);
+        },
+
+        getVapidPublicKeyFromServer: async function ()  {
+            const token = document.querySelector('meta[name=csrf-token]').getAttribute('content');
+
+            return await fetch('/webPush/vapidPublicKey', {
+                method: 'GET', headers: {
+                    'Accept': 'application/json', 'Content-Type': 'application/json', 'X-CSRF-Token': token
+                }
+            })
+                .then((res) => {
+                    return res.json();
+                })
+                .then((res) => {
+                     return res.public_key;
+                })
+                .catch((err) => {
+                    webPush.errors.push("Error while getting vapid public key from server.")
+                    console.log(err)
+                    return null;
+                });
+        },
+    },
+
+    subscription: {
+
+        getPushSubscription: () => {
+            return navigator.serviceWorker.ready.then(swr => {
+                return swr.pushManager.getSubscription().then(async sub => {
+                    const vapidPubKey = await webPush.getVapidPublicKey();
+                    if (webPush.urlBase64ToUint8Array(vapidPubKey).toString() === new Uint8Array(sub.options.applicationServerKey).toString()) {
+                        webPush.hasPushSubscription = true;
+                        return sub;
+                    }
+                    webPush.hasPushSubscription = false;
+                    return false;
+                });
+            }).catch(err => {
+                webPush.errors.push("unable to get service worker")
+                console.log(err);
+                webPush.hasPushSubscription = false;
+                return false;
+            });
+        },
+    },
+
+
     isPushManagerSupported() {
         return "PushManager" in window;
     }
--- resources/views/livewire/development.blade.php
+++ resources/views/livewire/development.blade.php
@@ -53,134 +53,165 @@
         </header>
     </section>
     <div class="shadow-xl shadow-black/5 sm:rounded-md bg-white p-3">
-        <ol class="text-sm p-2 text-gray-700">
+        <ol class="text-sm p-2 text-gray-700" x-data="{
+            isNotificationSupported: webPush.notification.isNotificationSupported(),
+            hasNotificationPermission: webPush.notification.hasNotificationPermission(),
+            notificationPermission: webPush.notification.getNotificationApiPermissionStatus(),
+            storedVapidPublicKey: webPush.vapid.getStoredVapidPublicKey(),
+            isPushManagerSupported: webPush.isPushManagerSupported(),
+            isServiceWorkerSupported: webPush.serviceWorker.isServiceWorkerSupported(),
+            hasServiceWorker: webPush.serviceWorker.hasServiceWorker(),
+            serviceWorker: navigator.serviceWorker.ready
+        }">
             <li>
-                <span class="font-bold">are notification supported:</span>
-                <span x-init x-text="webPush.notification.isNotificationSupported()"></span>
+                <span class="font-bold">notification supported:</span>
+                <span x-text="isNotificationSupported"></span>
             </li>
             <li>
                 <span class="font-bold">notification permission granted:</span>
-                <span x-init x-text="webPush.notification.hasNotificationPermission()"></span>
+                <span x-text="">hasNotificationPermission</span>
             </li>
             <li>
                 <span class="font-bold">notification api permission status:</span>
-                <span x-init x-text="webPush.notification.getNotificationApiPermissionStatus()"></span>
+                <span x-text="notificationPermission"></span>
             </li>
             <li>
-                <span class="font-bold">are service worker supported:</span>
-                <span x-init x-text="webPush.serviceWorker.isServiceWorkerSupported()"></span>
+                <span class="font-bold">stored vapid:</span>
+                <div class="text-xs ml-5 break-all" x-text="storedVapidPublicKey"></div>
             </li>
             <li>
-                <span class="font-bold">has active service worker:</span>
-                <span x-init x-text="webPush.serviceWorker.hasServiceWorker()"></span>
+                <span class="font-bold">push managers supported:</span>
+                <span x-text="isPushManagerSupported"></span>
             </li>
             <li>
-                <span class="font-bold">service worker script url:</span>
-                <span x-init x-text="webPush.serviceWorker.getServiceWorkerScriptUrl()"></span>
+                <span class="font-bold">service worker supported:</span>
+                <span x-text="isServiceWorkerSupported"></span>
             </li>
             <li>
-                <span class="font-bold">are push managers supported:</span>
-                <span x-init x-text="webPush.isPushManagerSupported()"></span>
+                <span class="font-bold">active service worker:</span>
+                <span x-text="hasServiceWorker"></span>
+            </li>
+            <template x-if="serviceWorker">
+                <li>
+                    <span class="font-bold">service worker script url:</span>
+                    <span x-text="webPush.serviceWorker.getServiceWorkerScriptUrl()"></span>
+                </li>
+                <li>
+                    <span class="font-bold">service worker has pushManager:</span>
+                    <span x-text="!!swr.pushManager"></span>
+                </li>
+                <template x-if="!!swr.pushManager" x-data="{ sub: swr.pushManager.getSubscription()}">
+                    <li>
+                        <span class="font-bold">pushManager has subscription:</span>
+                        <span x-text="!!sub"></span>
+                    </li>
+                </template>
+            </template>
+            <li x-cloak x-init x-show="webPush.errors.length">
+                <span class="font-bold">web push errors:</span>
+                <template x-for="error in webPush.errors">
+                    <div class="text-xs" x-text="error"></div>
+                </template>
             </li>
         </ol>
         <hr/>
-        <script>
-            function initWebPush() {
-                return {
-                    forceIsReady: null,
-                    isBrowserReady: null,
-                    hasServiceWorker: null,
-                    hasPushSubscription: null,
-                    isPushSubscriptionStored: null,
-                    init() {
-                        webPush.setupAll(true);
-                    },
-                    updateFlags() {
-                        this.forceIsReady = webPush.forceIsReady;
-                        this.isBrowserReady = webPush.isBrowserReady;
-                        this.hasServiceWorker = webPush.hasServiceWorker;
-                        this.hasPushSubscription = webPush.hasPushSubscription;
-                        this.isPushSubscriptionStored = webPush.isPushSubscriptionStored;
-                    }
-                }
-            }
-        </script>
-        <details open>
-            <summary>Web Push Notification Info</summary>
-            <div class="mx-2 mt-3">
-                <span>Legend: </span>
-                <i class='text-green-700 fa-solid fa-circle-check'></i> Passed
-                <i class='text-orange-700 fa-solid fa-triangle-exclamation'></i> Not Checked
-                <i class='text-red-700 fa-solid fa-circle-xmark'></i> Failed
-            </div>
-            <ol class="text-sm p-2 text-gray-700" x-data="initWebPush()"
-                x-on:webpush-setup-finished.window="updateFlags">
-                <li>
-                    <i :class="{
-                        'text-green-700 fa-solid fa-circle-check': Notification.permission === 'granted',
-                        'text-orange-700 fa-solid fa-triangle-exclamation': Notification.permission === 'default',
-                        'text-red-700 fa-solid fa-circle-xmark': Notification.permission === 'denied'}"></i> has
-                    notification permission
-                    (<span class="text-gray-500" x-text="Notification.permission"></span>)
-                </li>
-                <li x-data="{serviceWorkerSupported: ('serviceWorker' in navigator)}">
-                    <i :class="{
-                        'text-green-700 fa-solid fa-circle-check': serviceWorkerSupported,
-                        'text-red-700 fa-solid fa-circle-xmark': serviceWorkerSupported === false}"></i> are service
-                    worker supported by your browser
-                </li>
-                <li x-data="{pushManagerSupported: ('PushManager' in window)}">
-                    <i :class="{
-                        'text-green-700 fa-solid fa-circle-check': pushManagerSupported,
-                        'text-red-700 fa-solid fa-circle-xmark': pushManagerSupported === false}"></i> is the push
-                    manager supported by your browser
-                </li>
-                <li>
-                    <i :class="{
-                        'text-green-700 fa-solid fa-circle-check': isBrowserReady,
-                        'text-orange-700 fa-solid fa-triangle-exclamation': isBrowserReady === null,
-                        'text-red-700 fa-solid fa-circle-xmark': isBrowserReady === false}"></i> is browser ready
-                </li>
-                <li>
-                    <i :class="{
-                        'text-green-700 fa-solid fa-circle-check': forceIsReady,
-                        'text-red-700 fa-solid fa-circle-xmark': forceIsReady === false}"></i> checked is cached
-                    <template x-if="forceIsReady"><span class="text-xs"
-                                                        x-text="'('+(new Date(Number(webPush.getLastCheckTimeStamp())))+')'"></span>
-                    </template>
-                </li>
-                <li>
-                    <i :class="{
-                        'text-green-700 fa-solid fa-circle-check': hasServiceWorker,
-                        'text-orange-700 fa-solid fa-triangle-exclamation': hasServiceWorker === null,
-                        'text-red-700 fa-solid fa-circle-xmark': hasServiceWorker === false}"></i> has service worker
-                </li>
-                <li>
-                    <i :class="{
-                        'text-green-700 fa-solid fa-circle-check': hasPushSubscription,
-                        'text-orange-700 fa-solid fa-triangle-exclamation': hasPushSubscription === null,
-                        'text-red-700 fa-solid fa-circle-xmark': hasPushSubscription === false}"></i> has push
-                    subscription
-                </li>
-                <li>
-                    <i :class="{
-                        'text-green-700 fa-solid fa-circle-check': isPushSubscriptionStored,
-                        'text-orange-700 fa-solid fa-triangle-exclamation': isPushSubscriptionStored === null,
-                        'text-red-700 fa-solid fa-circle-xmark': isPushSubscriptionStored === false}"></i> is push
-                    subscription stored
-                </li>
-                <li>
-                    <i x-data="{isVapidKeyStored: webPush.isVapidPublicKeyStored()}" :class="{
-                        'text-green-700 fa-solid fa-circle-check': isVapidKeyStored,
-                        'text-orange-700 fa-solid fa-triangle-exclamation': isVapidKeyStored === null,
-                        'text-red-700 fa-solid fa-circle-xmark': isVapidKeyStored === false}"></i>
-                    <span>is vapid public key stored</span>
-                    <template x-if="webPush.isVapidPublicKeyStored">
-                        <div class="text-xs ml-5 break-all" x-text="webPush.getStoredVapidPublicKey()"></div>
-                    </template>
-                </li>
-            </ol>
-        </details>
+{{--        <script>--}}
+{{--            function initWebPush() {--}}
+{{--                return {--}}
+{{--                    forceIsReady: null,--}}
+{{--                    isBrowserReady: null,--}}
+{{--                    hasServiceWorker: null,--}}
+{{--                    hasPushSubscription: null,--}}
+{{--                    isPushSubscriptionStored: null,--}}
+{{--                    init() {--}}
+{{--                        webPush.setupAll(true);--}}
+{{--                    },--}}
+{{--                    updateFlags() {--}}
+{{--                        this.forceIsReady = webPush.forceIsReady;--}}
+{{--                        this.isBrowserReady = webPush.isBrowserReady;--}}
+{{--                        this.hasServiceWorker = webPush.hasServiceWorker;--}}
+{{--                        this.hasPushSubscription = webPush.hasPushSubscription;--}}
+{{--                        this.isPushSubscriptionStored = webPush.isPushSubscriptionStored;--}}
+{{--                    }--}}
+{{--                }--}}
+{{--            }--}}
+{{--        </script>--}}
+{{--        <details open>--}}
+{{--            <summary>Web Push Notification Info</summary>--}}
+{{--            <div class="mx-2 mt-3">--}}
+{{--                <span>Legend: </span>--}}
+{{--                <i class='text-green-700 fa-solid fa-circle-check'></i> Passed--}}
+{{--                <i class='text-orange-700 fa-solid fa-triangle-exclamation'></i> Not Checked--}}
+{{--                <i class='text-red-700 fa-solid fa-circle-xmark'></i> Failed--}}
+{{--            </div>--}}
+{{--            <ol class="text-sm p-2 text-gray-700" x-data="initWebPush()"--}}
+{{--                x-on:webpush-setup-finished.window="updateFlags">--}}
+{{--                <li>--}}
+{{--                    <i :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': Notification.permission === 'granted',--}}
+{{--                        'text-orange-700 fa-solid fa-triangle-exclamation': Notification.permission === 'default',--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': Notification.permission === 'denied'}"></i> has--}}
+{{--                    notification permission--}}
+{{--                    (<span class="text-gray-500" x-text="Notification.permission"></span>)--}}
+{{--                </li>--}}
+{{--                <li x-data="{serviceWorkerSupported: ('serviceWorker' in navigator)}">--}}
+{{--                    <i :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': serviceWorkerSupported,--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': serviceWorkerSupported === false}"></i> are service--}}
+{{--                    worker supported by your browser--}}
+{{--                </li>--}}
+{{--                <li x-data="{pushManagerSupported: ('PushManager' in window)}">--}}
+{{--                    <i :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': pushManagerSupported,--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': pushManagerSupported === false}"></i> is the push--}}
+{{--                    manager supported by your browser--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <i :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': isBrowserReady,--}}
+{{--                        'text-orange-700 fa-solid fa-triangle-exclamation': isBrowserReady === null,--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': isBrowserReady === false}"></i> is browser ready--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <i :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': forceIsReady,--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': forceIsReady === false}"></i> checked is cached--}}
+{{--                    <template x-if="forceIsReady"><span class="text-xs"--}}
+{{--                                                        x-text="'('+(new Date(Number(webPush.getLastCheckTimeStamp())))+')'"></span>--}}
+{{--                    </template>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <i :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': hasServiceWorker,--}}
+{{--                        'text-orange-700 fa-solid fa-triangle-exclamation': hasServiceWorker === null,--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': hasServiceWorker === false}"></i> has service worker--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <i :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': hasPushSubscription,--}}
+{{--                        'text-orange-700 fa-solid fa-triangle-exclamation': hasPushSubscription === null,--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': hasPushSubscription === false}"></i> has push--}}
+{{--                    subscription--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <i :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': isPushSubscriptionStored,--}}
+{{--                        'text-orange-700 fa-solid fa-triangle-exclamation': isPushSubscriptionStored === null,--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': isPushSubscriptionStored === false}"></i> is push--}}
+{{--                    subscription stored--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <i x-data="{isVapidKeyStored: webPush.isVapidPublicKeyStored()}" :class="{--}}
+{{--                        'text-green-700 fa-solid fa-circle-check': isVapidKeyStored,--}}
+{{--                        'text-orange-700 fa-solid fa-triangle-exclamation': isVapidKeyStored === null,--}}
+{{--                        'text-red-700 fa-solid fa-circle-xmark': isVapidKeyStored === false}"></i>--}}
+{{--                    <span>is vapid public key stored</span>--}}
+{{--                    <template x-if="webPush.isVapidPublicKeyStored">--}}
+{{--                        <div class="text-xs ml-5 break-all" x-text="webPush.getStoredVapidPublicKey()"></div>--}}
+{{--                    </template>--}}
+{{--                </li>--}}
+{{--            </ol>--}}
+{{--        </details>--}}
     </div>
     <div class="shadow-xl shadow-black/5 sm:rounded-md bg-white p-3">
         <details open>
