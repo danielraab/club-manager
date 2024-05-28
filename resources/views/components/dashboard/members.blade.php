bcd4ee35624249a6f3430abdc04eb20bef38b2e2
Daniel Raab <daniel@draab.at>
2024-05-28 11:22:21 +0200

    show message if member data is consistent

--- resources/views/components/dashboard/members.blade.php
+++ resources/views/components/dashboard/members.blade.php
@@ -5,11 +5,12 @@
 @endphp
 @if($hasShowPermission || $hasEditPermission)
     <div class="flex-1 flex flex-col mb-3">
-            @php($membersWithoutBirthday = \App\Models\Member::getAllFiltered()->whereNull('birthday'))
+        @php($membersWithoutBirthday = \App\Models\Member::getAllFiltered()->whereNull('birthday')->get())
         <div class="bg-white shadow-sm sm:rounded-lg p-4 text-center">
+            @if($membersWithoutBirthday->isNotEmpty())
             <header class="font-bold mb-3">{{__('Member without birthdays')}}</header>
             <ul class="grid text-sm gap-2 text-white">
-                @foreach($membersWithoutBirthday->get() as $member)
+                @foreach($membersWithoutBirthday as $member)
                     <li class="flex p-1 bg-red-600 rounded gap-4 justify-center">
                         <span>{{$member->getFullName()}}</span>
                         @if($hasEditPermission)
@@ -20,6 +21,9 @@
                     </li>
                 @endforeach
             </ul>
+            @else
+                <span class="text-gray-500">{{__('member data is consistent')}}</span>
+            @endif
         </div>
     </div>
 @endif
