387764121e0aef6239a8b0ef47c51af802294c14
Raab Daniel <raab.daniel@fronius.com>
2024-09-02 17:24:22 +0200

    add add buttons to dashboard (news/event)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,16 +7,16 @@
     </x-slot>
 
     <div class="flex flex-col md:flex-row gap-4">
-        <x-dashboard.members />
-        <x-dashboard.polls />
-        <x-dashboard.sponsoring />
+        <x-dashboard.members/>
+        <x-dashboard.polls/>
+        <x-dashboard.sponsoring/>
     </div>
-    <x-dashboard.lastBirthdays />
+    <x-dashboard.lastBirthdays/>
     <div class="flex flex-col-reverse lg:grid lg:grid-cols-2 gap-4">
         <div class="bg-white shadow-sm sm:rounded-lg p-4">
             <x-dashboard.eventList :eventList="$eventList"/>
-            <div class="flex flex-row items-center justify-around mt-3">
-                <a href="{{route('event.calendar')}}" class="btn btn-success max-md:hidden" title="Calendar">
+            <div class="flex flex-row items-center justify-around my-3">
+                <a href="{{route('event.calendar')}}" class="btn btn-info max-md:hidden" title="Calendar">
                     <i class="fa-solid fa-calendar-days mr-1"></i>
                     {{__("Calendar")}}
                 </a>
@@ -35,10 +35,26 @@
                     <span>{{route("event.iCalendar")}}</span>
                 </div>
             </div>
+            @if(\Illuminate\Support\Facades\Auth::user()?->hasPermission(\App\Models\Event::EVENT_EDIT_PERMISSION))
+                <div class="text-center mt-3">
+                    <a href="{{route('event.create')}}" class="btn btn-create" title="Create new event">
+                        <i class="fa-solid fa-plus mr-1"></i>
+                        {{__("Create new event")}}
+                    </a>
+                </div>
+            @endif
         </div>
         <div class="bg-white shadow-sm sm:rounded-lg p-4">
             <x-dashboard.newsList :newsList="$newsList"/>
-        </div>
 
+            @if(\Illuminate\Support\Facades\Auth::user()?->hasPermission(\App\Models\News::NEWS_EDIT_PERMISSION))
+                <div class="text-center mt-3">
+                    <a href="{{route('news.create')}}" class="btn btn-create" title="Create new news">
+                        <i class="fa-solid fa-plus mr-1"></i>
+                        {{__("Create new news")}}
+                    </a>
+                </div>
+            @endif
+        </div>
     </div>
 </x-backend-layout>
