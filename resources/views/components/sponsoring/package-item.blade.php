e9c7a1fbf64228dc890a6e5939249509945c8d1e
Raab Daniel <raab.daniel@fronius.com>
2024-10-24 16:29:35 +0200

    simplyfy package item compoenent

--- resources/views/components/sponsoring/package-item.blade.php
+++ resources/views/components/sponsoring/package-item.blade.php
@@ -4,35 +4,37 @@
     /** @var $period \App\Models\Sponsoring\Period */
 @endphp
 <div class="relative border bg-blue-300 py-2 px-4 rounded-md @if(!$package->enabled) text-gray-500 @endif"
-    x-data="{showOptions:true, showPeriods:false}">
+    x-data="{showOptions:true, showPeriods:false}"
+>
     <div class="flex justify-between items-center">
-        <h2 class="font-bold">{{$package->title}}</h2>
+        <h2 class="font-bold text-lg">{{$package->title}}</h2>
         @if(!$package->is_official)
             <div class="bg-red-700 text-white px-2 rounded-full text-sm">{{__("not official")}}</div>
         @endif
     </div>
     @if($package->description)
-        <p>{{$package->description}}</p>
+        <p class="text-gray-800 text-sm">{{$package->description}}</p>
     @endif
-    @if(($options = $package->adOptions()->get())->isNotEmpty())
-        <h3 @click="showOptions=!showOptions" class="font-semibold mt-3">{{__("Ad Options")}}
-            <i class="fa-solid"
-               :class="showOptions ? 'fa-caret-down' : 'fa-caret-right'"></i>
-        </h3>
-        <ul class="list-disc pl-5" x-cloak x-show="showOptions" x-collapse>
-             @foreach($options as $option)
-                 <li>{{$option->title}}</li>
-             @endforeach
-        </ul>
-    @else
-        <div class="bg-red-700 text-white rounded px-3 py-1 my-4 inline-block">{{__("no ad options")}}</div>
-    @endif
+
+    <p class="mt-3">
+        <span class="font-semibold inline-block min-w-[60px]">{{__("Ad Options")}}:</span>
+        @if(($options = $package->adOptions()->get())->isNotEmpty())
+            <ul class="list-disc pl-5 ml-2 text-sm">
+                 @foreach($options as $option)
+                     <li>{{$option->title}}</li>
+                 @endforeach
+            </ul>
+        @else
+            <div class="bg-red-700 text-white rounded px-3 py-1 my-4 inline-block">{{__("no ad options")}}</div>
+        @endif
+    </p>
     @if(($periods = $package->periods()->get())->isNotEmpty())
-        <h3 @click="showPeriods=!showPeriods" class="font-semibold mt-3 cursor-pointer">{{__("Periods")}}
+        <h3 @click="showPeriods=!showPeriods" class="font-semibold mt-3 cursor-pointer">
             <i class="fa-solid"
                :class="showPeriods ? 'fa-caret-down' : 'fa-caret-right'"></i>
+            {{__("Periods")}}
         </h3>
-        <ul class="list-disc pl-5" x-cloak x-show="showPeriods" x-collapse>
+        <ul class="list-disc pl-5 ml-2 text-sm" x-cloak x-show="showPeriods" x-collapse>
              @foreach($periods as $period)
                  <li>{{$period->title}}</li>
              @endforeach
